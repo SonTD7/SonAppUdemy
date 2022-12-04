8ee0fde95667354778150211cbb3cad7e16aac5b
SonTD <sontddeveloper@email.com>
2022-12-04 12:24:58 +0700

    commit

--- routes/shop.js
+++ routes/shop.js
@@ -8,13 +8,14 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 
-router.get('/products',shopController.getProducts);
+router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/orders',shopController.getOrders);
+router.get('/cart', shopController.getCart);
 
-router.get('/cart',shopController.getCart);
+router.get('/orders', shopController.getOrders);
 
-router.get('/checkout',shopController.getCheckout);
+router.get('/checkout', shopController.getCheckout);
+
 module.exports = router;
